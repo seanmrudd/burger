2d06db56ae5c4ae1465c56bba6870fdea7d7e2be
shrudder <seanmrudd@me.com>
2019-06-13 23:22:12 -0400

    initial commit, folders set up and db made

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
+INSERT INTO burgers (burger_name) VALUES ("Black and Blue");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Jalapeno");
