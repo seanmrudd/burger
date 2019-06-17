3727f93412125e89be6ae76338a99d106ed694bb
shrudder <seanmrudd@me.com>
2019-06-16 21:46:16 -0400

    have html showing up on localhost

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,51 @@
 var express = require("express");
 var router = express.Router();
+var Burger = require("../models/burger");
 
 router.get("/", function(request, response) {
-    
+    Burger.all(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        response.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(request, response) {
+    Burger.create([
+        "burger_name", "devoured"
+    ], [
+        request.body.burger_name, request.body.devoured
+    ], function(result) {
+        response.json({id: result.insertID});
+    });
+});
+
+router.put("/api/burgers/:id", function(request, response) {
+    var condition = "id = " + request.params.id;
+    console.log("condition", condition);
+    Burger.update({
+        devoured: request.body.devoured
+    }, condition, function(result){
+        if(result.changedRows == 0){
+            return result.status(404).end();
+        } else {
+            result.status(200).end();
+        };
+    });
+});
+
+router.delete("/api/burgers/:id", function(request, response){
+    var condition = "id = " + request.params.id;
+    Burger.update(condition, function(result){
+        if(result.affectedRows == 0) {
+            return result.status(400).end();
+        } else {
+            result.status(200).end();
+        }
+    })
 })
+
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,26 @@
 var orm = require("../config/orm");
 
-var Burgers = {
+var Burger = {
     all: function(cb) {
         orm.all("burgers", function(response) {
-            cb(res);
-        })
+            cb(response);
+        });
     },
     create: function (columns, values, cb) {
         orm.create("burgers", columns, values, function(response) {
             cb(response);
-        })
+        });
     },
-    update: function ()
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(response){
+            cb(response);
+        });
+    },
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function(response){
+            cb(response);
+        });
+    }
 }
+
+module.exports = Burger;
