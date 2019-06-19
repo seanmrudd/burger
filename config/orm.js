37ed4233e578e38a35e29d8a353c7d6d475d3ef0
seanRudd <seanmrudd@me.com>
2019-06-18 22:12:54 -0400

    push subtle changes to work on home desktop

--- config/orm.js
+++ config/orm.js
@@ -1,76 +1,52 @@
 var connection = require("./connection");
 
-function printQuestionMarks(num) {
-    var arr = [];
-  
-    for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
-    return arr.toString();
-  }
-  
-  function objToSql(ob) {
-    var arr = [];
-  
-    for (var key in ob) {
-      var value = ob[key];
-      if (Object.hasOwnProperty.call(ob, key)) {
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        arr.push(key + "=" + value);
+var orm = {
+  all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, tableInput, function (err, result) {
+      if (err) {
+        throw err;
       }
-    }
-      return arr.toString();
-  }
-  
-  var orm = {
-    all: function(tableInput, cb) {
-      var queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    create: function(table, cols, vals, cb) {
-      var queryString = "INSERT INTO burgers (burger) VALUES (?)";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, req.body.burgerName, function(err, result) {
-        if (err) {
-          return res.status(500).end();
-        }
-        cb(result);
-      });
-    },
-    update: function(table, objColVals, condition, cb) {
-      var queryString = "UPDATE burgers SET devour = true WHERE id = ?"
-  
-      console.log(queryString);
-      connection.query(queryString, req.params.id, function(err, result) {
-        if (err) {
-          return res.status(500).end();
-        }
-        cb(result);
-      });
-    },
-    delete: function(table, condition, cb) {
-      var queryString = "DELETE FROM " + table;
-      queryString += " WHERE ";
-      queryString += condition;
-  
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
-    }
-  };
+      cb(result);
+    });
+  },
+  create: function (cols, vals, cb) {
+    var queryString = "INSERT INTO burgers (burger) VALUES (?)";
 
-  module.exports = orm;
+    console.log(queryString);
+
+    connection.query(queryString, req.body.burgerName, function (err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+      cb(result);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    var queryString = "UPDATE burgers SET devoured = ? WHERE id = ?"
+
+    console.log(queryString);
+    connection.query(queryString, [true, req.params.id],
+      function (err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+      cb(result);
+    });
+  },
+  delete: function (table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
